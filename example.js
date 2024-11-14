54f394104827b6171d67690baacee4ca77e5cbb0
Eduardocantu02 <eduardo.cantu08@utrgv.edu>
2024-11-13 21:56:47 -0600

    Add files via upload

---
+++ example.js
@@ -0,0 +1,26 @@
+var listItems = document.getElementsByTagName('li');                   // All <li> elements
+
+// ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
+var heading = document.querySelector('h2');                        // h2 element
+var headingText = heading.firstChild.nodeValue;                    // h2 text
+var totalItems = listItems.length;                                 // No. of <li> elements
+var newHeading =  headingText + '<span>' + totalItems + '</span>'; // Content
+
+
+/*  WRITE YOUR CODE BELOW */
+
+
+// Part1. Hint: Use the listItems object to iterate through the elements of the list
+for (var i = 0; i < listItems.length; i++) {
+    var itemText = listItems[i].textContent.toLowerCase(); // Get text content of each item
+    
+    // Check if item is "fresh figs", "kale", or "honey"
+    if (itemText === 'fresh figs' || itemText === 'kale' || itemText === 'honey') {
+        listItems[i].className = 'cool'; // Change class to "cool" if match is found
+    }
+}
+// Part2. Hint: Use the heading and newHeading to show the number of items in the shopping list
+heading.innerHTML = newHeading;
+
+
+
